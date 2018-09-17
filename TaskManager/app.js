375547c8474550e7f200fc024bcd03cd0be4bc85
Kojo Addaquay <kojoaddaquay@gmail.com>
2018-09-16 22:17:50 -0400

    added functionality to filter and delete tasks

--- TaskManager/app.js
+++ TaskManager/app.js
@@ -10,6 +10,9 @@ function loadEventListeners(){
 
   // Add tasks
   form.addEventListener('submit', addTask);
+  filter.addEventListener('keyup', filterTasks);
+  taskList.addEventListener('click', removeTask);
+  clearTasks.addEventListener('click', clearAllTasks);
 }
 
 function addTask(e){
@@ -20,11 +23,9 @@ function addTask(e){
     alert("enter something!");
   } else {
     // create list item
-    const taskItem = document.createTextNode(task.value);
-
     const li = document.createElement("li");
     li.className = 'collection-item';
-    li.appendChild(taskItem);
+    li.appendChild(document.createTextNode(task.value));
 
     //Add remove icons
     const link = document.createElement("a");
@@ -38,7 +39,53 @@ function addTask(e){
   }
 }
 
-// Delete Tasks
+//filter tasks
+
+function filterTasks(e){
+  const text = e.target.value.toLowerCase();
+  const allTasks = document.querySelectorAll('.collection-item');
+  allTasks.forEach(function(task){
+    const item = task.firstChild.textContent;
+    //show task if it matches filter params
+    if(item.toLowerCase().indexOf(text) != -1){
+     task.style.display = "block";
+    } else {
+    //hide task if it doesn't
+      task.style.display = "none";
+    }
+  });
+}
+
+// Delete Each Task
+
+function removeTask(e){
+  removeItem = document.querySelector('.fa-remove');
+  if (e.target === removeItem){
+    if(confirm('Are you sure?')){
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+// Delete all tasks
+
+function clearAllTasks(e){
+  if (e.target.classList.contains('clear-tasks')) {
+    if(confirm('Are you sure??')){
+      const allTasks = document.querySelectorAll('.collection-item');
+      allTasks.forEach(function(task){
+        task.remove();
+      });
+    }
+  }
+ //  *faster method*
+ //   while(taskList.firstChild){
+ //  taskList.removeChild(taskList.firstChild);
+ // }
+}
+
+
+
 
 
 
